75c407d9212c2f4540503e9db55072eb28284859
Juan Carlos Hernández <jchernandez0312@gmail.com>
2021-09-27 17:32:35 -0500

    ✨ Initial commit

---
+++ src/test/java/com/phonesubscription/PhonesubscriptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phonesubscription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonesubscriptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
