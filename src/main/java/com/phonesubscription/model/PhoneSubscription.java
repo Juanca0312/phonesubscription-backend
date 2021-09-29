73d893a982a8f205a021f62febbbb981bdff51aa
Juan Carlos Hernández <jchernandez0312@gmail.com>
2021-09-28 20:09:20 -0500

    ✨ Created PhoneSubscription model

---
+++ src/main/java/com/phonesubscription/model/PhoneSubscription.java
@@ -0,0 +1,35 @@
+package com.phonesubscription.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Entity
+@Table(name = "phone_subscriptions")
+@Data
+public class PhoneSubscription {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    private Date month;
+
+    @NotBlank
+    @NotNull
+    private String network_technology;
+
+    @NotNull
+    @NotBlank
+    private String plan_type;
+
+    @NotNull
+    @NotBlank
+    private int subscriptions;
+
+
+}
